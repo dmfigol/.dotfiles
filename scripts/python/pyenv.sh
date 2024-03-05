865741d8d89c175da9db7c7dd8806a9a452a40f7
Dmitry Figol <git@dmfigol.me>
2024-03-05 15:41:09 +0100

    Update pyenv.sh

--- scripts/python/pyenv.sh
+++ scripts/python/pyenv.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-PYENV_MACOS_PATCH_URL="https://github.com/python/cpython/commit/8ea6353.patch\?full_index\=1"
-
 function python_install {
   # install pyenv if doesn't exist
   if ! command -v pyenv &> /dev/null; then
@@ -12,12 +10,7 @@ function python_install {
   pyenv_global=""
   for python_version in $@
   do
-    # if [ `uname` = "Darwin" ] && [ $python_version = "3.6" ]; then
-    #   # patch for macOS
-    #   pyenv install -s --patch $python_version:latest < <(curl -sSL $PYENV_MACOS_PATCH_URL)
-    # else
-      pyenv install -s $python_version:latest
-    # fi
+    pyenv install -s $python_version:latest
     pyenv_global+="$(pyenv versions --bare | grep ${python_version} | tail -1) "
     python$python_version -m pip install -U pip
   done
